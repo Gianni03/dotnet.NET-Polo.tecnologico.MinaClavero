5a8b70da91a5a48dfcad64d6d7375e8cc540c23e
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-06-19 14:55:47 -0300

    Primeros pasos en C# para el curso de .NET

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+//datos numéricos
+
+byte num_byte = 0;
+int num_int= 123;
+long num_long= 121231233;
+double num_double= 123.343;
+float num_float= 12.3f;
+decimal num_decimal= 12.3m;
+
+// Console.WriteLine(num_byte,num_int,num_long,num_double,num_float);
+Console.WriteLine(num_byte);
+Console.WriteLine(num_int);
+Console.WriteLine(num_long);
+Console.WriteLine(num_double);
+Console.WriteLine(num_float);
+
+//dato lógico
+bool datoBool= false;
+Console.WriteLine(datoBool);
+
+//date texto
+string datoString = "texto";
+Console.WriteLine(datoString);
+datoString = "texto de prueba";
+Console.WriteLine(datoString);
+
+
---
+++ demoVSC/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//datos numéricos
+
+byte num_byte = 0;
+int num_int= 123;
+long num_long= 121231233;
+double num_double= 1233.43;
+float num_float= 1223.23f;
+decimal num_decimal= 1223.23m;
+
+Console.WriteLine(num_byte);
+Console.WriteLine(num_int);
+Console.WriteLine(num_long);
+Console.WriteLine(num_double);
+Console.WriteLine(num_float);
+Console.WriteLine(num_decimal);
+
+
+//dato lógico
+bool datoBool= false;
+Console.WriteLine(datoBool);
+
+//date texto
+string datoString = "texto";
+Console.WriteLine(datoString);
+datoString = "texto de prueba";
+Console.WriteLine(datoString);
+
+
+Console.ReadLine();
