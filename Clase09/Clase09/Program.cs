bad73d1b566adcb9be24c091ac6b565ad7f842da
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-06-29 23:44:16 -0300

    trabajando con arrays - clase 09

---
+++ Clase09/Clase09/Program.cs
@@ -0,0 +1,49 @@
+﻿
+Console.Write("Tamaño del listado: ");
+int n = int.Parse(Console.ReadLine());
+int[] vector = new int[n];
+Random aleatorio = new Random();//variable de tipo random
+for (int i = 0; i < vector.Length; i++)
+{//llenamos la matriz con valores random hasta el num 100
+  vector[i] = aleatorio.Next(100);
+}
+
+var contador = 0;
+foreach (int i in vector)
+{//mostramos los valores
+  contador++;
+  Console.Write(contador + ": " + i + " ");
+}
+Console.WriteLine();
+Console.Write("Posicion a modificar: ");
+var k = int.Parse(Console.ReadLine());
+if (k > vector.Length || k < 0)
+{
+  Console.WriteLine("Indice incorrecto");
+}
+else
+{
+  Console.Write("Ingrese un nuevo valor: ");
+  while (true)
+  {
+    var nuevoValor = int.Parse(Console.ReadLine());
+    if (nuevoValor > 0 && nuevoValor < 100)
+    {
+      vector[k - 1] = nuevoValor;
+      break;//para salir del bucle infinito
+    }
+    else
+    {
+      Console.WriteLine("El valor debe ser positivo y menor de 100");
+    }
+  }
+  
+}
+contador = 0;
+foreach (int i in vector)
+{//mostramos los valores
+  contador++;
+  Console.Write(contador + ": " + i + " ");
+}
+
+
---
+++ Clase09/Primos/Primos/Program.cs
@@ -0,0 +1,30 @@
+﻿//acer un array de 100 elementos que muestre lo números primos
+Console.Write("Cuantos numeros primos quieres: ");
+var n = int.Parse(Console.ReadLine());
+
+int[] primos = new int[n];
+int posicion = 0;
+int aux = 0;
+int numero = 1;
+while (posicion < n)
+{
+  aux = 0;
+  for (int i = 1; i <= numero; i++)
+  {
+    if (numero % i == 0 )
+    {
+      aux++;
+    }
+  }
+  if (aux == 2)
+  {
+    primos[posicion] = numero;
+    posicion++;
+  }
+  numero++;
+}
+
+foreach (var i in primos)
+{
+  Console.WriteLine(i);
+}
